b40f4bced9039bca1cfbd0d449b637e19337bbdf
Val-Antin1 <valentinlyon205@gmail.com>
2026-01-15 13:12:15 +0200

    backend updated

--- createAdmin.js
+++ createAdmin.js
@@ -9,7 +9,7 @@ async function createAdmin() {
     console.log('MongoDB connected');
 
     // Check if admin already exists
-    const existingAdmin = await Admin.findOne({ email: 'valentinlyon205@gmail.com' });
+    const existingAdmin = await Admin.findOne({ email: 'kingbanks133@gmail.com' });
     if (existingAdmin) {
       console.log('Admin user already exists');
       process.exit(0);
@@ -17,14 +17,14 @@ async function createAdmin() {
 
     // Create admin user
     const admin = new Admin({
-      email: 'valentinlyon205@gmail.com',
-      password: 'mamannkunda', // This will be hashed by the pre-save hook
+      email: 'kingbanks133@gmail.com',
+      password: 'mama_shema', // This will be hashed by the pre-save hook
     });
 
     await admin.save();
     console.log('Admin user created successfully');
-    console.log('Email: valentinlyon205@gmail.com');
-    console.log('Password: mamannkunda');
+    console.log('Email: kingbanks133@gmail.com');
+    console.log('Password: mama_shema');
 
   } catch (error) {
     console.error('Error creating admin:', error);
